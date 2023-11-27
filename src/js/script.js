cbd8e5e6238d8e4a9bf4fc4f5654062b8e96c34e
TED Vortex (Teodor-Eugen Duțulescu) <237133+0-vortex@users.noreply.github.com>
2023-11-27 21:00:14 +0200

    fix: correct embed responsiveness on post page (#6)
    * fix: correct embed responsiveness on post page
    
    * make youtube embeds bigger
    
    ---------
    
    Co-authored-by: Catalin Pit <catalinpit@gmail.com>

--- src/js/script.js
+++ src/js/script.js
@@ -1,138 +1,147 @@
-jQuery(function($) {
-
-  var html = $('html');
+jQuery(function ($) {
+  var html = $("html");
   var viewport = $(window);
 
-/* ==========================================================================
+  function video() {
+    var post = $(".post-content");
+    post.fitVids();
+  }
+
+  video();
+
+  /* ==========================================================================
    Menu
    ========================================================================== */
 
   function menu() {
-    html.toggleClass('menu-active');
-  };
+    html.toggleClass("menu-active");
+  }
 
-  $('#menu').on({
-    'click': function() {
+  $("#menu").on({
+    click: function () {
       menu();
-    }
+    },
   });
 
-  $('.nav-menu').on({
-    'click': function() {
+  $(".nav-menu").on({
+    click: function () {
       menu();
-    }
+    },
   });
 
-  $('.nav-close').on({
-    'click': function() {
+  $(".nav-close").on({
+    click: function () {
       menu();
-    }
+    },
   });
 
   viewport.on({
-    'resize': function() {
-      html.removeClass('menu-active');
+    resize: function () {
+      html.removeClass("menu-active");
     },
-    'orientationchange': function() {
-      html.removeClass('menu-active');
-    }
+    orientationchange: function () {
+      html.removeClass("menu-active");
+    },
   });
 
-/* ==========================================================================
+  /* ==========================================================================
    Parallax cover
    ========================================================================== */
 
-  var cover = $('.cover');
+  var cover = $(".cover");
   var coverPosition = 0;
 
   function prlx() {
     if (cover.length >= 1) {
       var windowPosition = viewport.scrollTop();
-      (windowPosition > 0) ? coverPosition = Math.floor(windowPosition * 0.25): coverPosition = 0;
+      windowPosition > 0
+        ? (coverPosition = Math.floor(windowPosition * 0.25))
+        : (coverPosition = 0);
       cover.css({
-        '-webkit-transform': 'translate3d(0, ' + coverPosition + 'px, 0)',
-        'transform': 'translate3d(0, ' + coverPosition + 'px, 0)'
+        "-webkit-transform": "translate3d(0, " + coverPosition + "px, 0)",
+        transform: "translate3d(0, " + coverPosition + "px, 0)",
       });
-      (viewport.scrollTop() < cover.height()) ? html.addClass('cover-active'): html.removeClass('cover-active');
+      viewport.scrollTop() < cover.height()
+        ? html.addClass("cover-active")
+        : html.removeClass("cover-active");
     }
   }
   prlx();
 
   viewport.on({
-    'scroll': function() {
+    scroll: function () {
       prlx();
     },
-    'resize': function() {
+    resize: function () {
       prlx();
     },
-    'orientationchange': function() {
+    orientationchange: function () {
       prlx();
-    }
+    },
   });
 
-/* ==========================================================================
+  /* ==========================================================================
    Gallery
    ========================================================================== */
 
   function gallery() {
-    'use strict';
-    var images = document.querySelectorAll('.kg-gallery-image img');
-    images.forEach(function(image) {
-      var container = image.closest('.kg-gallery-image');
+    "use strict";
+    var images = document.querySelectorAll(".kg-gallery-image img");
+    images.forEach(function (image) {
+      var container = image.closest(".kg-gallery-image");
       var width = image.attributes.width.value;
       var height = image.attributes.height.value;
       var ratio = width / height;
-      container.style.flex = ratio + ' 1 0%';
+      container.style.flex = ratio + " 1 0%";
     });
   }
   gallery();
 
-
-/* ==========================================================================
+  /* ==========================================================================
    Theme
    ========================================================================== */
 
   function theme() {
-    'use strict';
-    var toggle = $('.js-theme');
-    var toggleText = toggle.find('.theme-text');
+    "use strict";
+    var toggle = $(".js-theme");
+    var toggleText = toggle.find(".theme-text");
 
     function system() {
-      html.removeClass(['theme-dark', 'theme-light']);
-      localStorage.removeItem('attila_theme');
-      toggleText.text(toggle.attr('data-system'));
+      html.removeClass(["theme-dark", "theme-light"]);
+      localStorage.removeItem("attila_theme");
+      toggleText.text(toggle.attr("data-system"));
     }
 
     function dark() {
-      html.removeClass('theme-light').addClass('theme-dark');
-      localStorage.setItem('attila_theme', 'dark');
-      toggleText.text(toggle.attr('data-dark'));
+      html.removeClass("theme-light").addClass("theme-dark");
+      localStorage.setItem("attila_theme", "dark");
+      toggleText.text(toggle.attr("data-dark"));
     }
 
     function light() {
-      html.removeClass('theme-dark').addClass('theme-light');
-      localStorage.setItem('attila_theme', 'light');
-      toggleText.text(toggle.attr('data-light'));
+      html.removeClass("theme-dark").addClass("theme-light");
+      localStorage.setItem("attila_theme", "light");
+      toggleText.text(toggle.attr("data-light"));
     }
 
-    switch (localStorage.getItem('attila_theme')) {
-      case 'dark':
+    switch (localStorage.getItem("attila_theme")) {
+      case "dark":
         dark();
-      break;
-      case 'light':
+        break;
+      case "light":
         light();
-      break;
+        break;
       default:
         system();
-      break;
+        break;
     }
 
-    toggle.on('click', function (e) {
+    toggle.on("click", function (e) {
       e.preventDefault();
 
-      if (!html.hasClass('theme-dark') && !html.hasClass('theme-light')) {
+      if (!html.hasClass("theme-dark") && !html.hasClass("theme-light")) {
         dark();
-      } else if (html.hasClass('theme-dark')) {
+      } else if (html.hasClass("theme-dark")) {
         light();
       } else {
         system();
